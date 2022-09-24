20bdbb8af9b395d2f08ddb908e1fbb90b4976634
34ruby <ixia5027@gmail.com>
2022-09-24 13:13:53 +0900

    220924 mobile - created 02_if_else

---
+++ bj/02_if_else/9498.py
@@ -0,0 +1,12 @@
+testNum = int(input())
+
+if testNum >= 90:
+    print('A')
+elif testNum >= 80:
+    print('B')
+elif testNum >= 70:
+    print('C')
+elif testNum >= 60:
+    print('D')
+else:
+    print('F')
